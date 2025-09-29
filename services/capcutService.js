e0512afb3ebb2381287731913aa69549e6e6647a
Samir Thakuri <itssamir444@gmail.com>
2025-09-29 12:39:19 +0545

    Update CapCut API endpoint and request headers

--- services/capcutService.js
+++ services/capcutService.js
@@ -2,37 +2,34 @@ const axios = require("axios");
 
 async function fetchCapcutData(url) {
   try {
-    const apiUrl = "https://3bic.com/api/download/";
+    const apiUrl = "https://www.genviral.io/api/tools/social-downloader";
 
     const response = await axios.post(
       apiUrl,
       { url },
       {
         headers: { 
-            'accept': 'application/json, text/plain, */*', 
-            'accept-language': 'en-US,en;q=0.9,fr;q=0.8,ne;q=0.7,ja;q=0.6', 
-            'content-type': 'application/json', 
-            'origin': 'https://3bic.com', 
-            'priority': 'u=1, i', 
-            'referer': 'https://3bic.com/', 
-            'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Google Chrome";v="140"', 
-            'sec-ch-ua-mobile': '?0', 
-            'sec-ch-ua-platform': '"Windows"', 
-            'sec-fetch-dest': 'empty', 
-            'sec-fetch-mode': 'cors', 
-            'sec-fetch-site': 'same-origin', 
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
+          'accept': '*/*', 
+          'accept-language': 'en-US,en;q=0.5', 
+          'baggage': 'sentry-environment=production,sentry-release=102ca3483de9fd3f3fc19cd9c61e8923bdab7852,sentry-public_key=360a5271964ef3bc33b47f8760ecec7d,sentry-trace_id=477e8e97116373a0dca68d1509192b89,sentry-org_id=4509345024901120,sentry-transaction=GET%20%2Ftools%2Fdownload%2F%5Bplatform%5D,sentry-sampled=true,sentry-sample_rand=0.0037737885879629562,sentry-sample_rate=1', 
+          'content-type': 'application/json', 
+          'dnt': '1', 
+          'origin': 'https://www.genviral.io', 
+          'priority': 'u=1, i', 
+          'referer': 'https://www.genviral.io/tools/download/capcut', 
+          'sec-ch-ua': '"Chromium";v="140", "Not=A?Brand";v="24", "Brave";v="140"', 
+          'sec-ch-ua-mobile': '?0', 
+          'sec-ch-ua-platform': '"Windows"', 
+          'sec-fetch-dest': 'empty', 
+          'sec-fetch-mode': 'cors', 
+          'sec-fetch-site': 'same-origin', 
+          'sec-gpc': '1', 
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36'
         },
       }
     );
 
-    return {
-        code: response.data.code,
-        title: response.data.title,
-        videoUrl: `https://3bic.com${response.data.originalVideoUrl}`,
-        coverUrl: response.data.coverUrl,
-        author: response.data.authorName,
-    };
+    return response.data;
   } catch (error) {
     throw new Error(`Capcut API request failed: ${error.message}`);
   }
